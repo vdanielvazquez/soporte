97cfcf32400b1b89fe86b7349944a3149f60ca66
Daniel1590@dan <v.danielvazquez.m@gmail.com>
2026-04-22 12:29:39 -0700

    titulo

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import EliminarCuenta from "./pages/EliminarCuenta";
 import TerminosYCondiciones from "./pages/TerminosYCondiciones";
 
@@ -6,9 +6,12 @@ export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<TerminosYCondiciones />} />
+        <Route path="/" element={<EliminarCuenta />} />
         <Route path="/eliminar-cuenta" element={<EliminarCuenta />} />
         <Route path="/terminos" element={<TerminosYCondiciones />} />
+
+        {/* fallback SIEMPRE al final */}
+        <Route path="*" element={<Navigate to="/terminos" />} />
       </Routes>
     </BrowserRouter>
   );
