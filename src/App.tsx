96442c3b3d83ea33ce0f7271db454c75466361d8
Daniel1590@dan <v.danielvazquez.m@gmail.com>
2026-04-22 12:31:18 -0700

    titulo

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<EliminarCuenta />} />
+        <Route path="/" element={<TerminosYCondiciones />} />
         <Route path="/eliminar-cuenta" element={<EliminarCuenta />} />
         <Route path="/terminos" element={<TerminosYCondiciones />} />
 
